7dcc955665e4382a121de96084e189911670a248
karldamus <getaholdofkarl@gmail.com>
2024-02-07 17:56:21 -0500

    Create RTree.py

---
+++ src/modules/RTree.py
@@ -0,0 +1,20 @@
+# RTree.py
+#
+# The RTree data structure spatial indexing module responsible for:
+# - Building the RTree
+# - Inserting data into the RTree
+# - Querying the RTree
+# - Removing data from the RTree
+#
+# ----------------
+#
+# AUTHOR: Karl Damus
+# DATE: 2024-02-01
+#
+# CHANGELOG:
+#
+# 2024-02-01: Created file
+#
+# ----------------
+#
+
