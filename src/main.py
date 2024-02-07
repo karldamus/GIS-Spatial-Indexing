7f11a591fde3e65b871a4a00c5657029878122d1
karldamus <getaholdofkarl@gmail.com>
2024-02-07 17:54:06 -0500

    Create main.py

---
+++ src/main.py
@@ -0,0 +1,25 @@
+# main.py
+# 
+# The main module responsible for:
+# - Data loading
+# - Indexing
+# - Querying
+# - Evaluation
+#
+# ----------------
+#
+# AUTHOR: Karl Damus
+# DATE: 2024-02-01
+#
+# CHANGELOG:
+#
+# 2024-02-01: Created file
+#
+# ----------------
+#
+
+def main():
+	pass
+
+if __name__ == "__main__":
+	main()
