487e20dfbab5fb0486f75b1c60f510c761a00628
karldamus <getaholdofkarl@gmail.com>
2024-02-07 17:58:28 -0500

    Create Quadtree.py

---
+++ src/modules/Quadtree.py
@@ -0,0 +1,22 @@
+# Quadtree.py
+#
+# The Quadtree data structure spatial indexing module responsible for:
+# - Building the Quadtree
+# - Inserting data into the Quadtree
+# - Querying the Quadtree
+# - Removing data from the Quadtree
+#
+# ----------------
+#
+# AUTHOR: Karl Damus
+# DATE: 2024-02-01
+#
+# CHANGELOG:
+#
+# 2024-02-01: Created file
+#
+# ----------------
+#
+
+class Quadtree:
+	pass
